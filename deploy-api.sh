5c16dd679430a9a84e131cdc44d3cc0a7658aa26
vkomodey <vkomodey@gmail.com>
2018-10-31 23:21:24 +0300

    add release commands

--- deploy-api.sh
+++ deploy-api.sh
@@ -1,2 +1,5 @@
 cd server/
+echo "Pushing:"
 heroku container:push web --remote vkomodey-dict-api
+echo "Releasing:"
+heroku container:release web --remote vkomodey-dict-api
--- deploy-web.sh
+++ deploy-web.sh
@@ -1,2 +1,5 @@
 cd client/
+echo "Pushing:"
 heroku container:push web --remote web
+echo "Releasing:"
+heroku container:release web --remote web
