f61b951c2dee25f2e7d990a5f01086aeda014a5f
vkomodey <vkomodey@gmail.com>
2018-11-04 01:25:31 +0300

    Remove heroku staff

--- deploy-api.sh
+++
@@ -1,5 +0,0 @@
-cd server/
-echo "Pushing:"
-heroku container:push web --remote vkomodey-dict-api
-echo "Releasing:"
-heroku container:release web --remote vkomodey-dict-api
--- deploy-web.sh
+++
@@ -1,5 +0,0 @@
-cd client/
-echo "Pushing:"
-heroku container:push web --remote web
-echo "Releasing:"
-heroku container:release web --remote web
